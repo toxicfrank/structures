02949300fd328c42a110b081aac4fc52bb855708
toxicfrank <ngugifrancis930@gmail.com>
2023-05-02 07:26:31 +0100

    practice

--- user.c
+++ user.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-struct human
+
+typedef struct human
 {
 	char *name;
 	float height;
 	int weight;
-};
+}human;
 
 int main(void)
 {
-	struct human h = {"James", 175.6, 80};
+	human h = {"James", 175.6, 80};
 	printf("%s %.2f %d\n", h.name, h.height, h.weight);
 
 	return (0);
